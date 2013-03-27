e2d2ecc2347ad30b403fb6ffeceb113bbfa1fd03
pierrick <pierrick@localhost.localdomain>
2013-03-27 16:23:31 +0100

    All code write for the moment

---
+++ meduson_db/admin.py
@@ -0,0 +1,61 @@
+from django.contrib import admin
+from meduson_db.models import Materiel
+from meduson_db.models import Domaine
+from meduson_db.models import Personne
+from meduson_db.models import Type
+from meduson_db.models import Accessoire
+from meduson_db.models import Client
+from meduson_db.models import Periode
+from meduson_db.models import Periode_Projet
+from meduson_db.models import Projet
+from meduson_db.models import Liste_Materiel
+from meduson_db.models import Location_Materiel
+from meduson_db.models import Prestation
+from meduson_db.models import Location_Prestation
+from meduson_db.models import Devis_Societe
+from meduson_db.models import Devis_Association
+from meduson_db.models import Facture_Societe
+from meduson_db.models import Facture_Association
+from meduson_db.models import Reparation
+
+admin.site.register(Domaine)
+admin.site.register(Type)
+admin.site.register(Personne)
+
+class AccessoireInline(admin.StackedInline):
+    model = Accessoire
+    extra = 5
+
+class MaterielAdmin(admin.ModelAdmin):
+    inlines = [AccessoireInline]
+
+admin.site.register(Materiel,MaterielAdmin)
+admin.site.register(Reparation)
+admin.site.register(Client)
+
+class PeriodeProjetInline(admin.StackedInline):
+    model = Periode_Projet
+    extra = 5
+
+class ProjetAdmin(admin.ModelAdmin):
+    inlines = [PeriodeProjetInline]
+
+admin.site.register(Projet,ProjetAdmin)
+admin.site.register(Liste_Materiel)
+admin.site.register(Prestation)
+
+class LocationMaterielInline(admin.StackedInline):
+    model = Location_Materiel
+    extra = 5
+
+class LocationPrestationInline(admin.StackedInline):
+    model = Location_Prestation
+    extra = 3
+
+class DevisFactureAdmin(admin.ModelAdmin):
+    inlines = [LocationMaterielInline,LocationPrestationInline]
+
+admin.site.register(Devis_Societe,DevisFactureAdmin)
+admin.site.register(Devis_Association,DevisFactureAdmin)
+
+
---
+++ meduson_db/models.py
@@ -0,0 +1,161 @@
+from django.db import models
+from datetime import timedelta
+
+# Create your models here.
+
+class Personne(models.Model):
+    nom = models.CharField(max_length = 80)
+    prenom = models.CharField(max_length = 80)
+    email = models.EmailField(max_length = 50, blank=True)
+    telephone = models.CharField(max_length = 20, blank=True)
+
+    def __unicode__(self):
+        return self.prenom + ' ' + self.nom
+
+class Type(models.Model):
+    nom = models.CharField(max_length = 50)
+    code = models.IntegerField(max_length=3,primary_key=True)
+    abreviation = models.CharField(max_length=50,blank=True)
+
+    def __unicode__(self):
+        return self.nom
+
+
+class Domaine(models.Model):
+    nom = models.CharField(max_length = 20)
+    code = models.CharField(max_length = 1,primary_key=True)
+    
+    def __unicode__(self):
+        return self.nom
+
+class Materiel(models.Model):
+    ETAT_CHOICE=(
+            ('OK','Ok'),
+            ('AR','A reparer'),
+            ('EM','En maintenance'),
+            ('DO','Douteux'),
+            ('MO','Mort'),
+            )
+    reference_meduson = models.CharField(max_length=8,primary_key=True)
+    domaine = models.ManyToManyField(Domaine)
+    pays = models.CharField(max_length=50,blank=True)
+    marque = models.CharField(max_length=50)
+    modele = models.CharField(max_length=50,blank=True)
+    numero_serie = models.CharField(max_length=20,blank=True)
+    annee = models.CharField(max_length=4)
+    proprietaire = models.ForeignKey(Personne)
+    valeur = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
+    prix_location_jour=models.DecimalField(max_digits=7,decimal_places=2,blank=True,null=True)
+    commentaire = models.CharField(max_length=300,blank=True)
+    etat = models.CharField(max_length=2,choices=ETAT_CHOICE)
+    type = models.ForeignKey(Type)
+    
+    def __unicode__(self):
+        return self.reference_meduson
+
+class Accessoire(models.Model):
+    nom = models.CharField(max_length = 50)
+    materiel = models.ForeignKey(Materiel)
+    
+    def __unicode__(self):
+        return self.nom
+
+class Client(models.Model):
+    numero = models.CharField(max_length=7,primary_key=True)
+    societe = models.CharField(max_length=50)
+    nom = models.CharField(max_length=50,blank=True)
+    prenom = models.CharField(max_length=50,blank=True)
+    adresse_postale = models.CharField(max_length=100,blank=True)
+    ville = models.CharField(max_length=100,blank=True)
+    code_postal = models.CharField(max_length=10,blank=True)
+    pays = models.CharField(max_length=50,blank=True)
+    telephone1 = models.CharField(max_length=20,blank=True)
+    telephone2 = models.CharField(max_length=20,blank=True)
+    abreviation = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.societe
+
+class Periode(models.Model):
+    debut = models.DateField()
+    fin = models.DateField()
+
+    def __unicode__(self):
+        return str(self.debut) + ' - ' + str(self.fin)
+
+
+class Projet(models.Model):
+    client = models.ForeignKey(Client)
+    referent = models.ForeignKey(Personne)
+
+    def __unicode__(self):
+        return self.client.societe + ' - ' + str(self.id)
+
+class Periode_Projet(Periode):
+    projet = models.ForeignKey(Projet)
+
+class Liste_Materiel(models.Model):
+    projet = models.ForeignKey(Projet)
+    materiel = models.ManyToManyField(Materiel)
+    periode = models.ForeignKey(Periode)
+
+    def __unicode__(self):
+        return self.projet.client.societe + ' - ' + str(self.periode.debut) + '-' +str(self.periode.fin)
+
+
+
+class Devis_Facture(models.Model):
+    domaine = models.ManyToManyField(Domaine)
+    projet = models.ForeignKey(Projet)
+    date = models.DateField()
+    date_paiement = models.DateField()
+    totalHT = models.DecimalField(max_digits=10,decimal_places=2)
+    acompte = models.DecimalField(max_digits=10,decimal_places=2,blank=True,null=True)
+    paye = models.BooleanField()
+   
+
+class Location_Materiel(models.Model):
+    materiel = models.ForeignKey(Materiel)
+    devis_facture = models.ForeignKey(Devis_Facture)
+    remise = models.IntegerField(blank=True,null=True)
+    quantite = models.IntegerField()
+    nb_jour = models.IntegerField()
+
+class Prestation(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.DecimalField(max_digits=10,decimal_places=2)
+
+class Location_Prestation(models.Model):
+    devis_facture = models.ForeignKey(Devis_Facture)
+    prestation = models.ForeignKey(Prestation)
+    quantite = models.IntegerField()
+
+class Devis_Societe(Devis_Facture):
+    id_spec = models.AutoField(unique=True,primary_key=True)
+
+    def __unicode__(self):
+        return 'DEVS'+str(self.id_spec)
+
+class Devis_Association(Devis_Facture):
+    id_spec = models.AutoField(unique=True,primary_key=True)
+    
+    def __unicode__(self):
+        return 'DEVA'+str(self.id_spec)
+
+
+class Facture_Societe(Devis_Facture):
+    id_spec = models.AutoField(unique=True,primary_key=True)
+    
+    def __unicode__(self):
+        return 'FACS'+str(self.id_spec)
+
+class Facture_Association(Devis_Facture):
+    id_spec = models.AutoField(unique=True,primary_key=True)
+    
+    def __unicode__(self):
+        return 'FACA'+str(self.id_spec)
+
+class Reparation(models.Model):
+    date = models.DateField()
+    commentaire = models.CharField(max_length = 200)
+    materiel = models.ForeignKey(Materiel)
